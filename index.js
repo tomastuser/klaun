e33435dea6222c0ea6fceef844c65a3c1d178cbd
tomastuser <tomas.tuser@ixperta.com>
2021-12-01 00:09:48 +0100

    sin, zoom, arrows

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let x, y;
+let newSizeRatio = 0;
+let inc;
+const speedRatio = 350;
+let angle = 0;
 const mx = 8.77;
 const my = 7.795;
-let pos = 0;
-let vel = 0;
-let acc = 0.005;
 const canvasWidth = 1600;
 const canvasHeight = 900;
 let nabytek;
 let step = 0;
-let centerX = 13;
-let centerY = 935;
+let centerX = 0;
+let centerY = 885;
 let test = false;
 const w = 2306;
 const h = 1403;
@@ -17,53 +18,63 @@ let introCounter = 255;
 let isInPositiveMotion = false;
 let isInNegativeMotion = false;
 let initialStepSetup = true;
-let img;
-let music;
-let intro = true;
+let img, arrowL, arrowR, music, auto, autostart;
+let intro = false;
+// let intro = true;
 let leftArrowHover = false;
 let rightArrowHover = false;
 let introArrowHover = false;
 let fadeOut = false;
 
 const positions = [
-  { centerX: 13, centerY: 935, controlX: 54, controlY: 171 },
-  { centerX: 1258, centerY: 1442, controlX: 14, controlY: 264 },
-  { centerX: 22, centerY: 3406, controlX: 108, controlY: 331 },
-  { centerX: 1548, centerY: 2580, controlX: 334, controlY: 360 },
-  { centerX: 4293, centerY: 2627, controlX: 477, controlY: 129 },
-  { centerX: 3556, centerY: 935, controlX: 150, controlY: 464 },
-  { centerX: 17 * mx, centerY: 690 * my, controlX: 48, controlY: 609 }, // WRONG
-  { centerX: 2337, centerY: 5121, controlX: 323, controlY: 556 },
-  { centerX: 4328, centerY: 4451, controlX: 456, controlY: 832 },
-  { centerX: 7020, centerY: 5355, controlX: 887, controlY: 499 },
-  { centerX: 6959, centerY: 3578, controlX: 682, controlY: 373 },
-  { centerX: 784 * mx, centerY: 80 * my, controlX: 971, controlY: 70 }, // WRONG
-  { centerX: 8906, centerY: 125, controlX: 1181, controlY: 386 },
-  { centerX: 8520, centerY: 1653, controlX: 1162, controlY: -40 },
-  { centerX: 10905, centerY: 413, controlX: 1326, controlY: 210 },
-  { centerX: 11204, centerY: 2494, controlX: 1291, controlY: 397 },
-  { centerX: 8994, centerY: 2697, controlX: 1017, controlY: 500 },
-  { centerX: 1038 * mx, centerY: 606 * my, controlX: 1142, controlY: 541 }, // WRONG
-  { centerX: 10458, centerY: 4007, controlX: 1340, controlY: 520 },
-  { centerX: 11221, centerY: 4700, controlX: 119, controlY: 142 },
+  { centerX: 0, centerY: 885, controlX: 54, controlY: 171, size: 1.1 },
+  { centerX: 1230, centerY: 1400, controlX: 14, controlY: 264, size: 1.05 },
+  { centerX: 0, centerY: 3440, controlX: 108, controlY: 331, size: 1.05 },
+  { centerX: 1480, centerY: 2480, controlX: 334, controlY: 360, size: 1.1 },
+  { centerX: 4293, centerY: 2627, controlX: 477, controlY: 129, size: 1 },
+  { centerX: 3556, centerY: 935, controlX: 150, controlY: 464, size: 1 },
+  { centerX: 0, centerY: 4850, controlX: 48, controlY: 609, size: 1.4 },
+  { centerX: 2240, centerY: 5100, controlX: 323, controlY: 556, size: 1.15 },
+  { centerX: 3400, centerY: 4000, controlX: 456, controlY: 832, size: 1.3 },
+  { centerX: 6750, centerY: 5355, controlX: 887, controlY: 499, size: 1.1 },
+  { centerX: 6959, centerY: 3500, controlX: 682, controlY: 373, size: 1.15 },
+  { centerX: 5675, centerY: 440, controlX: 971, controlY: 70, size: 1.4 },
+  { centerX: 8906, centerY: 125, controlX: 1181, controlY: 386, size: 1 },
+  { centerX: 8520, centerY: 1653, controlX: 1162, controlY: -40, size: 0.9 },
+  { centerX: 10905, centerY: 413, controlX: 1326, controlY: 210, size: 1 },
+  { centerX: 11204, centerY: 2094, controlX: 1291, controlY: 397, size: 1.2 },
+  { centerX: 8994, centerY: 2697, controlX: 1017, controlY: 500, size: 1 },
+  { centerX: 8503, centerY: 4373, controlX: 1142, controlY: 541, size: 1.33 },
+  { centerX: 10358, centerY: 3707, controlX: 1340, controlY: 520, size: 1.2 },
+  { centerX: 11221, centerY: 4600, controlX: 119, controlY: 142, size: 1.1 },
+  { centerX: 0, centerY: 0, controlX: 3556, controlY: 935, size: 5 },
 ];
 
 function preload() {
   img = loadImage('k.jpg');
+  arrowL = loadImage('arrowL.png');
+  arrowR = loadImage('arrowR.png');
+  img = loadImage('k.jpg');
   soundFormats('mp3');
   music = loadSound('hud');
+  music.setVolume(0.2);
+  // auto = loadSound('autoj');
+  // autostart = loadSound('auto');
   fontBold = loadFont('public/calibri-regular.ttf');
 }
 
 function setup() {
   var canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('sketch-holder');
+  inc = PI / speedRatio;
   x = positions[step].centerX;
   y = positions[step].centerY;
+  newSizeRatio = positions[0].size - 1;
   //   nabytek = new Ratio(1, 128, 320, 180, '    ');
 }
 
 function draw() {
+  background(255);
   if (intro) {
     noStroke();
     fill(255, 255, 255);
@@ -76,18 +87,24 @@ function draw() {
     text('Projeďte si s námi', 800, 350);
     text('naši 20 letou cestu smíchu!', 800, 450);
     fill(introArrowHover ? 255 : 55, 77, 137);
-    text('->', 800, 580);
+    image(arrowR, 700, 580);
+    // text('->', 800, 580);
     textAlign(LEFT);
-    fill(0, introCounter);
-    rect(0, 0, 1600, 900);
+
     if (introCounter > 0) {
+      fill(0, sin((introCounter / 255) * HALF_PI) * 255);
       introCounter -= 2;
-    }
+    } else fill(0, 0);
     if (fadeOut && introCounter < 500) {
+      if (introCounter > 255) {
+        fill(0, 255);
+      }
       introCounter += 6;
     }
+    rect(0, 0, 1600, 900);
     if (introCounter >= 500) {
       music.play();
+      // autostart.play();
       introCounter = 255;
       intro = false;
     }
@@ -96,67 +113,69 @@ function draw() {
       introCounter -= 2;
     }
     if (isInPositiveMotion) {
-      if (pos === 100) {
+      if (angle > HALF_PI) {
         step++;
-        pos = 0;
+        angle = 0;
         isInPositiveMotion = false;
       } else {
-        if (pos < 99.95) {
-          pos += vel;
-        } else {
-          pos = 100;
-        }
-        if (pos < 50.1 && vel < 1) {
-          vel = Number((vel + acc).toFixed(3));
-        } else if (pos > 50.1 && vel > 0) {
-          vel = Number((vel - acc).toFixed(3));
-        }
+        angle += inc;
         x = bezierPoint(
           positions[step].centerX,
           positions[step].centerX,
           positions[step].controlX * mx,
           positions[step + 1].centerX,
-          pos / 100
+          sin(sin(angle) * angle)
         );
         y = bezierPoint(
           positions[step].centerY,
           positions[step].centerY,
           positions[step].controlY * my,
           positions[step + 1].centerY,
-          pos / 100
+          sin(sin(angle) * angle)
         );
+        if (positions[step].size > positions[step + 1].size) {
+          newSizeRatio =
+            (positions[step].size - positions[step + 1].size) *
+              (1 - sin(sin(angle) * angle)) +
+            (positions[step + 1].size - 1);
+        } else
+          newSizeRatio =
+            (positions[step + 1].size - positions[step].size) *
+              sin(sin(angle) * angle) +
+            (positions[step].size - 1);
       }
     }
     if (isInNegativeMotion) {
-      if (pos === 100) {
+      if (angle > HALF_PI) {
         step--;
-        pos = 0;
+        angle = 0;
         isInNegativeMotion = false;
       } else {
-        if (pos < 99.95) {
-          pos += vel;
-        } else {
-          pos = 100;
-        }
-        if (pos < 50.1 && vel < 1) {
-          vel = Number((vel + acc).toFixed(3));
-        } else if (pos > 50.1 && vel > 0) {
-          vel = Number((vel - acc).toFixed(3));
-        }
+        angle += inc;
         x = bezierPoint(
           positions[step].centerX,
           positions[step].centerX,
           positions[step - 1].centerX,
           positions[step - 1].centerX,
-          pos / 100
+          sin(sin(angle) * angle)
         );
         y = bezierPoint(
           positions[step].centerY,
           positions[step].centerY,
           positions[step - 1].centerY,
           positions[step - 1].centerY,
-          pos / 100
+          sin(sin(angle) * angle)
         );
+        if (positions[step].size > positions[step - 1].size) {
+          newSizeRatio =
+            (positions[step].size - positions[step - 1].size) *
+              (1 - sin(sin(angle) * angle)) +
+            (positions[step - 1].size - 1);
+        } else
+          newSizeRatio =
+            (positions[step - 1].size - positions[step].size) *
+              sin(sin(angle) * angle) +
+            (positions[step].size - 1);
       }
     }
 
@@ -177,8 +196,23 @@ function draw() {
         (positions[step + 1].centerY + h / 2) / my
       );
     } else {
-      image(img, 0, 0, canvasWidth, canvasHeight, x, y, 2807, 1403);
-      fill(0, introCounter);
+      image(
+        img,
+        0,
+        0,
+        canvasWidth,
+        canvasHeight,
+        x,
+        y,
+        // x + cos(angle * 20) * 1.5,
+        // y + sin(radians(millis() + random(20))) * 3,
+        // y + sin(angle * 40) * 3,
+        2807 * (1 + newSizeRatio),
+        1403 * (1 + newSizeRatio)
+      );
+      if (introCounter > 0) {
+        fill(0, sin((introCounter / 255) * HALF_PI) * 255);
+      } else fill(0, 0);
       rect(0, 0, canvasWidth, canvasHeight);
     }
 
@@ -192,19 +226,22 @@ function draw() {
     // text(centerY, 100, 400);
     // text(mouseX.toFixed(0), 20, 350);
     // text(mouseY.toFixed(0), 20, 400);
-    // text(x, 20, 450);
+    // tkext(x, 20, 450);
     // text(y, 20, 500);
     // text(pos, 20, 650);
+    textSize(64);
+    fill(0);
+    // text(sin(angle), 20, 650);
+    // text(isInPositiveMotion, 20, 550);
+    // text(newSizeRatio, 20, 600);
     // text(step, 800, 50);
-    // textSize(64);
     fill(leftArrowHover ? 255 : 55, 77, 137);
-    text('<-', 750, 860);
+    image(arrowL, 730, 820, 121, 74);
+    // text('<-', 750, 860);
     fill(55, 77, 137);
     fill(rightArrowHover ? 255 : 55, 77, 137);
-    text('->', 850, 860);
-    //   nabytek.display();
-    //   text(pos, 100, 450);
-    //   text(vel, 100, 500);
+    image(arrowR, 870, 820, 121, 74);
+    // text('->', 850, 860);
   }
 }
 
@@ -224,39 +261,50 @@ function mouseMoved() {
   }
 }
 
+function moveRight() {
+  if (test && step < 19) {
+    step++;
+  } else if (test && step >= 19) {
+    step = 0;
+  } else if (!isInNegativeMotion && step < 20) {
+    inc = PI / (speedRatio + random(100));
+    // auto.play();
+    isInPositiveMotion = true;
+  }
+}
+
+function moveLeft() {
+  if (test && step > 0) {
+    step--;
+  } else if (test && step <= 0) {
+    step = 19;
+  } else if (!isInPositiveMotion && step > 0) {
+    inc = PI / (speedRatio + random(100));
+    // auto.play();
+    isInNegativeMotion = true;
+  }
+}
+
 function mousePressed() {
-  //   if (nabytek.check()) {
-  //     nabytek.dragged = true;
-  //   }
-  if (intro && mouseX > 780 && mouseX < 820 && mouseY > 550 && mouseY < 600) {
+  if (intro && mouseX > 680 && mouseX < 820 && mouseY > 550 && mouseY < 700) {
     fadeOut = true;
   }
   if (
-    mouseX > 730 &&
+    mouseX > 720 &&
     mouseX < 805 &&
     mouseY > 810 &&
     mouseY < 900 &&
     step > 0
   ) {
-    if (test) {
-      step--;
-    } else {
-      pos = 0;
-      isInNegativeMotion = true;
-    }
+    moveLeft();
   } else if (
-    mouseX > 820 &&
-    mouseX < 900 &&
+    mouseX > 830 &&
+    mouseX < 940 &&
     mouseY > 810 &&
     mouseY < 900 &&
-    step < 19
+    step < 20
   ) {
-    if (test) {
-      step++;
-    } else {
-      pos = 0;
-      isInPositiveMotion = true;
-    }
+    moveRight();
   }
 }
 
@@ -268,65 +316,9 @@ function keyTyped() {
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    if (test && step > 0) {
-      step--;
-    } else if (test && step <= 0) {
-      step = 18;
-    } else {
-      pos = 0;
-      isInNegativeMotion = true;
-    }
+    moveLeft();
   }
   if (keyCode === RIGHT_ARROW) {
-    if (test && step < 18) {
-      step++;
-    } else if (test && step >= 18) {
-      step = 0;
-    } else {
-      pos = 0;
-      isInPositiveMotion = true;
-    }
+    moveRight();
   }
 }
-
-// function mouseReleased() {
-//   nabytek.dragged = false;
-// }
-
-// class Ratio {
-//   constructor(x_, y_, a_, b_, name_) {
-//     this.x = x_;
-//     this.y = y_;
-//     this.a = a_;
-//     this.b = b_;
-//     this.c = 0;
-//     this.dragged = false;
-//     this.name = name_;
-//   }
-//   display() {
-//     strokeWeight(2);
-//     stroke(0);
-//     noFill();
-//     if (this.dragged == true) {
-//       this.x = mouseX;
-//       this.y = mouseY;
-//       rect(this.x, this.y, this.a, this.b, this.c);
-//     } else {
-//       rect(this.x, this.y, this.a, this.b, this.c);
-//     }
-//     strokeWeight(0);
-//     fill(0);
-//     text(this.name, this.x, this.y + 6);
-//   }
-//   check() {
-//     let above = false;
-//     let dx = abs(mouseX - this.x);
-//     let dy = abs(mouseY - this.y);
-//     if (dx < this.a / 2 && dy < 5) {
-//       above = true;
-//     } else {
-//       above = false;
-//     }
-//     return above;
-//   }
-// }
